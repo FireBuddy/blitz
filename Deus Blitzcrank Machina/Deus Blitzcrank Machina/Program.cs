565179c88dacbc4d3ef4fd4d61a052d9d04d7c7d
AkumaCORE <k-sintu@hotmail.com>
2016-05-21 16:11:21 -0400

    Update Program.cs

--- Deus Blitzcrank Machina/Deus Blitzcrank Machina/Program.cs
+++ Deus Blitzcrank Machina/Deus Blitzcrank Machina/Program.cs
@@ -95,7 +95,7 @@ namespace Deus_Blitzcrank_Machina
                return;
             }
 
-            if (Sender.IsValidTarget(Q.Range) && Q.IsReady() && !Sender.IsAlly && !Sender.IsMe && !Sender.IsMinion && !Sender.IsMonster)
+            if (GameObjectType.obj_AI_Hero && Sender.IsValidTarget(Q.Range) && Q.IsReady() && !Sender.IsAlly && !Sender.IsMe && !Sender.IsMinion && !Sender.IsMonster)
             {
                 {
                     Q.Cast(Sender);
